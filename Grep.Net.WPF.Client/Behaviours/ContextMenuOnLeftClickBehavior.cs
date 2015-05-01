36a92ede99f9d12bf185dcd978019534a27a6dda
John Hernandez <john.hern@outlook.com>
2015-05-01 07:04:13 -0700

    Adding drop down support to the title menu. This will be used to support things like Options or Manage to reduce clutter on the main UI.

---
+++ Grep.Net.WPF.Client/Behaviours/ContextMenuOnLeftClickBehavior.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Interactivity;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace Grep.Net.WPF.Client.Behaviours
+{
+    public class ContextMenuOnLeftClickBehavior : Behavior<ButtonBase>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.Click += OnClick;
+        }
+
+        protected override void OnDetaching()
+        {
+            AssociatedObject.Click -= OnClick;
+            base.OnDetaching();
+        }
+
+        private void OnClick(object sender, RoutedEventArgs routedEventArgs)
+        {
+            var contextMenu = AssociatedObject.ContextMenu;
+            if (contextMenu == null)
+            {
+                return;
+            }
+
+            contextMenu.PlacementTarget = AssociatedObject;
+            contextMenu.Placement = PlacementMode.Bottom;
+            contextMenu.IsOpen = true;
+        }
+    }
+}
